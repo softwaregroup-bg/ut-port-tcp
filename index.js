4bebc65d630d44dd5f636592aa5b514441543505
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2014-12-18 14:12:50 +0200

    initial implementation

--- index.js
+++ index.js
@@ -3,90 +3,82 @@
     var when = require('when');
     var net = require('net');
     var bitSyntax = require('bitsyntax');
-    var logOut = '';
-    var logIn = '';
-    var level = {};
-    var log = null;
+    var Port = require('ut-bus/port');
+    var util = require('util');
+    var utcodec = require('ut-codec');
 
-    return function TcpPort() {
-        //private fields
-        var conn = null;
-        var framePattern = null;
-        var codec = null;
+    function TcpPort() {
+        Port.call(this);
+        this.conn = null;
+        this.framePattern = null;
+        this.codec = null;
+        this.config = {
+            id: null,
+            logLevel: '',
+            type: 'tcp',
+            host: '127.0.0.1',
+            port: null,
+            listen: false,
+            format: {
+                size: null,
+                codec: null,
+                id: null
+            }
+        };
+    }
 
-        function Receiver(callback) {
-            var buffer = new Buffer(0);
+    util.inherits(TcpPort, Port);
 
-            return function(packet) {
-                buffer = Buffer.concat([buffer, packet]);
-                console.log('packet:', packet, 'buffer:', buffer.toString());
+    function Receiver(port) {
+        var buffer = new Buffer(0);
 
-                if (framePattern) {
-                    var frame;
-                    while (frame = framePattern(buffer)) {
-                        buffer = frame.rest;
-                        callback(codec ? codec.decode(frame.data) : {payload:frame.data});
-                    }
+        return function processPacket(packet) {
+            buffer = Buffer.concat([buffer, packet]);
+            this.level.trace && this.log.trace(packet);
+
+            if (this.framePattern) {
+                var frame;
+                while (frame = this.framePattern(buffer)) {
+                    buffer = frame.rest;
+                    this.receive(this.codec ? this.codec.decode(frame.data) : {payload:frame.data});
                 }
-            };
-        }
+            }
+        }.bind(port);
+    }
 
-        return {
-            bus: null,
-            utcodec: null,
-            config: {
-                id: null,
-                type: 'tcp',
-                host: '127.0.0.1',
-                port: null,
-                listen: false,
-                format: {
-                    size: null,
-                    codec: null,
-                    id: null
-                }
-            },
-            init: function() {
-                logOut = 'out ' + this.id + ':';
-                logIn = 'in  ' + this.id + ':';
-                (log = this.logger) && (level = log.initLevels(this.logLevel));
-                var methods = {};
-                methods['ports.' + this.config.id + '.start'] = this.start;
-                methods['ports.' + this.config.id + '.stop'] = this.stop;
-                if (this.bus) {
-                    this.bus.register(methods);
-                }
-                if (this.config.format) {
-                    if (this.config.format.size) {
-                        framePattern = bitSyntax.matcher('len:' + this.config.format.size + ', data:len/binary, rest/binary');
-                    }
-                    if (this.config.format.codec) {
-                        var x = this.utcodec.get(this.config.format.codec);
-                        codec = new x({});
-                    }
-                }
-            },
-            start: function(callback) {
-                if (this.config.listen) {
-                    conn = net.createServer(function(c) {
-                        c.on('data', new Receiver(this.receive.bind(this)));
-                    }.bind(this));
-                    conn.listen(this.config.port);
-                } else {
-                    conn = net.createConnection({port:this.config.port, host:this.config.host}, function(c) {
-                        c.on('data', new Receiver(this.receive.bind(this)));
-                    }.bind(this));
-                }
-            },
+    TcpPort.prototype.init = function init() {
+        Port.prototype.init.apply(this, arguments);
 
-            stop: function(callback) {
-
-            },
-
-            receive: function(msg) {
-                console.log('message:', msg);
+        if (this.config.format) {
+            if (this.config.format.size) {
+                this.framePattern = bitSyntax.matcher('len:' + this.config.format.size + ', data:len/binary, rest/binary');
+            }
+            if (this.config.format.codec) {
+                var x = utcodec.get(this.config.format.codec);
+                this.codec = new x(this.config.format);
             }
         }
-    }
+    };
+
+    TcpPort.prototype.start = function start(callback) {
+        Port.prototype.start.apply(this, arguments);
+
+        if (this.config.listen) {
+            this.conn = net.createServer(function(c) {
+                c.on('data', new Receiver(this));
+            }.bind(this));
+            this.conn.listen(this.config.port);
+        } else {
+            this.conn = net.createConnection({port:this.config.port, host:this.config.host}, function(c) {
+                c.on('data', new Receiver(this));
+            }.bind(this));
+        }
+    };
+
+    TcpPort.prototype.receive = function(msg) {
+        this.level.debug && this.log.debug(msg);
+    };
+
+    return TcpPort;
 
 });}(typeof define === 'function' && define.amd ? define : function(factory) { module.exports = factory(require); }));
--- test/testNDC.js
+++ test/testNDC.js
@@ -1,9 +1,40 @@
-var tcp = require('../');
-var c = new tcp();
-c.utcodec = require('ut-codec');
-c.config.port = 5000;
-c.config.listen = true;
-c.config.format.size = '16/integer';
-c.config.format.codec = 'ndc';
-c.init();
-c.start();
+require('repl').start({useGlobal: true});
+
+var wire = require('wire');
+
+m = wire({
+    bunyan: {
+        create: {
+            module: 'ut-log',
+            args: {
+                type: 'bunyan',
+                name: 'bunyan_test',
+                streams: [
+                    {
+                        level: 'trace',
+                        stream: 'process.stdout'
+                    }
+                ]
+            }
+        }
+    },
+    atm: {
+        create: 'ut-port-tcp',
+        init: 'init',
+        properties: {
+            config: {
+                id: 'atm',
+                logLevel: 'trace',
+                port: 5000,
+                listen: true,
+                format: {
+                    size: '16/integer',
+                    codec: 'ndc'
+                }
+            },
+            log: {$ref: 'bunyan'}
+        }
+    }
+}, {require: require}).then(function contextLoaded(context) {
+    context.atm.start();
+}).done();
