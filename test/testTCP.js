f96138bb8efccbaf7d90b92b8854ba4627d44ce1
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2015-02-06 16:08:56 +0200

    refactor to allow modular structure

--- test/testTCP.js
+++
@@ -1,9 +0,0 @@
-require('repl').start({useGlobal:true});
-
-var tcp = require('../');
-c = new tcp();
-c.config.port = 5000;
-c.config.listen = true;
-c.config.sizeFormat = '32/integer';
-c.init();
-c.start();
